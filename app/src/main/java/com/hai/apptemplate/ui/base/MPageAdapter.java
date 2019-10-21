7ae4fa434ebc0370f874b468c093b61173cea07f
huanghp <1132760021@qq.com>
2019-10-21 17:15:41 +0800

    update basePageAdapter

--- app/src/main/java/com/hai/apptemplate/ui/base/MPageAdapter.java
+++ app/src/main/java/com/hai/apptemplate/ui/base/MPageAdapter.java
@@ -1,5 +1,6 @@
 package com.hai.apptemplate.ui.base;
 
+import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,15 +10,15 @@ import java.util.List;
 /**
  * viewpage适配器
  */
-public class MPageAdapter extends PagerAdapter {
-    private List<View> views;
+public class MPageAdapter<T extends View> extends PagerAdapter {
+    private List<T> views;
 
     @Override
     public int getCount() {
-        return views.size();
+        return views != null ? views.size() : 0;
     }
 
-    public MPageAdapter(List<View> views) {
+    public MPageAdapter(List<T> views) {
         super();
         this.views = views;
     }
@@ -28,12 +29,12 @@ public class MPageAdapter extends PagerAdapter {
     }
 
     @Override
-    public void destroyItem(ViewGroup container, int position, Object object) {
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
         container.removeView(views.get(position));
     }
 
     @Override
-    public Object instantiateItem(ViewGroup container, int position) {
+    public T instantiateItem(ViewGroup container, int position) {
         container.addView(views.get(position));
         return views.get(position);
     }
